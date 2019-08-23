7b560cb861599fb6ccb243a5c23237dea6063673
VFerrari <vferrari@mpc.com.br>
2019-08-23 08:13:46 -0300

    Little changes

--- project_0/p0.py
+++ project_0/p0.py
@@ -1,46 +1,47 @@
-import cv2
+from cv2 import imwrite, imread
+from os import splitText, basename
 import numpy as np
 
 # Part 1
 # Image negative
 def negative(filename):
-	img = 255 - cv2.imread(filename, 0)
-	cv2.imwrite('Outputs/negative.png', img)
+	img = 255 - imread(filename, 0)
+	imwrite('Outputs/negative.png', img)
 
 # Converting intensity interval to [100,200]
 def intensity(filename):
-	img = cv2.imread(filename, 0)
+	img = imread(filename, 0)
 	img = np.uint8(((img/255) * 100) + 100)
-	cv2.imwrite('Outputs/intensity.png', img)
+	imwrite('Outputs/intensity.png', img)
 
 # Converting intensity interval to [100,200] of the negative.
 def transform_intensity(filename):
-	img = 255 - cv2.imread(filename, 0)
+	img = 255 - imread(filename, 0)
 	img = np.uint8(((img/255) * 100) + 100)
-	cv2.imwrite('Outputs/transform_intensity.png', img)
+	imwrite('Outputs/transform_intensity.png', img)
 
 # Part 2
 # Adjusting brightness given gamma.
 def brightness(filename, gamma):
-	img = cv2.imread(filename,0)/255
+	img = imread(filename,0)/255
 	img = img**(1/gamma)
 	img = np.uint8(img*255)
-	cv2.imwrite('Outputs/brightness_' + str(gamma) + '.png', img)
+	imwrite('Outputs/brightness_' + str(gamma) + '.png', img)
 
 # Part 3
 # Extract bit plane of a monochromatic image.
 def bit_plane(filename, order):
-	img   = cv2.imread(filename, 0)
+	img   = imread(filename, 0)
 	img = ((img >> order) % 2)*255
-	cv2.imwrite('Outputs/bit_plane_' + str(order) + '.png', img)
+	imwrite('Outputs/bit_plane_' + str(order) + '.png', img)
 	
 # Part 4
 # 4x4 mosaic from a monochromatic image
 # Assumes square image (and possible to do a 4x4 mosaic), receives new order.
 def mosaic(filename, arrange):
-	img     = cv2.imread(filename, 0)
+	img     = imread(filename, 0)
 	arrange = np.array(arrange)
-	p_len   = np.uint16(img.shape[0]/4)
+	p_len   = img.shape[0]//4
 	
 	# Split image in 16 blocks
 	i=0
@@ -55,12 +56,12 @@ def mosaic(filename, arrange):
 	for i in range(4):
 		rows[i] = np.concatenate(blocks[arrange[i]], axis=1)
 	img = np.concatenate(rows)
-	cv2.imwrite('Outputs/mosaic.png', img)
+	imwrite('Outputs/mosaic.png', img)
 
 # Part 5
 # Combine 2 images by weighted average
 def combine(filename1, filename2, wei1, wei2):
-	img1 = cv2.imread(filename1, 0)
-	img2 = cv2.imread(filename2, 0)
+	img1 = imread(filename1, 0)
+	img2 = imread(filename2, 0)
 	ans  = wei1*img1 + wei2*img2
-	cv2.imwrite('Outputs/combine_' + str(wei1) + '_' + str(wei2) + '.png', ans)
+	imwrite('Outputs/combine_' + str(wei1) + '_' + str(wei2) + '.png', ans)
