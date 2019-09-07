ef021d1fc235007cbd3e6aecdc716de79703ff03
VFerrari <vferrari@mpc.com.br>
2019-09-07 11:10:59 -0300

    Improving code

--- project_1/p1.py
+++ project_1/p1.py
@@ -1,4 +1,4 @@
-from cv2 import imwrite, imread
+from cv2 import imwrite, imread, copyMakeBorder, BORDER_CONSTANT
 from math import floor
 import os
 import numpy as np
@@ -7,31 +7,41 @@ import numpy as np
 # Receives the image and the error distribution (mask)
 def dithering(img, error_dist, alt):
     
+    # Variables for border and error diffusion
+    sizex = floor(error_dist.shape[1]/2)
+    sizey = error_dist.shape[0] - 1
+    
+    # Create border on image
+    img = copyMakeBorder(img, top=0, bottom=sizey, left=sizex, right=sizex, borderType = BORDER_CONSTANT, value = 0)
+    
     # Half-toned image
-    out = np.zeros((img.shape[0],img.shape[1]))
+    shapeY,shapeX = img.shape
+    endX = shapeX - sizex
+    endY = shapeY - sizey
+    out = np.zeros((shapeY,shapeX))
     
     # Apply to the entire image
-    for y in range(img.shape[0]):
+    for y in range(endY):
         
         # Reverse if odd indexed line
         reverse = (y % 2 == 1) if alt else False
         
-        for x in range(img.shape[1])[::-1 if reverse else 1]:
+        for x in range(sizex, endX)[::-1 if reverse else 1]:
             out[y,x] = 255 * floor(img[y,x]/128)
             
             # Error diffusion
             diff = int(img[y,x]) - int(out[y,x])
-            sizex = floor(error_dist.shape[1]/2)
-            sizey =  floor(error_dist.shape[0]/2)
-            for i in range(sizey + 1):
-                if y+i < img.shape[0]:
-                    for j in range(-sizex, sizex+1):
-                        if x+j < img.shape[1] and x+j >= 0:
-                            img[y+i,x+j]+=error_dist[i,j + sizex-1]*diff
+            for i in range(sizey+1):
+                for j in range(-sizex, sizex+1):
+                    img[y+i,x+j]+=error_dist[i,j + sizex-1]*diff
+    
+    # Removing border
+    out = out[:endY,sizex:endX]
     return out
             
-
+# Halftones an image via dithering with error diffusion.
 def halftoning(filename, error_dist, alternating):
     img = imread(filename, 0)
+    error_dist = np.array(error_dist)
     img = dithering(img, error_dist, alternating)
     imwrite("Outputs/test.png", img)
