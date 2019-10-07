7482afdbe41e28ed1dbd53d14fe7b215c939eba4
VFerrari <vferrari@mpc.com.br>
2019-10-07 15:38:07 -0300

    Changing names for script file of projects 1 and 2

--- project_1/run.sh
+++
@@ -1,9 +0,0 @@
-python3 p1.py ../Images/monochromatic/baboon.png all --zig --mono
-python3 p1.py ../Images/monochromatic/butterfly.png all --zig --mono
-python3 p1.py ../Images/monochromatic/city.png all --zig --mono
-python3 p1.py ../Images/monochromatic/house.png all --zig --mono
-python3 p1.py ../Images/monochromatic/seagull.png all --zig --mono
-python3 p1.py ../Images/color/baboon.png all --zig
-python3 p1.py ../Images/color/monalisa.png all --zig
-python3 p1.py ../Images/color/peppers.png all --zig
-python3 p1.py ../Images/color/watch.png all --zig
